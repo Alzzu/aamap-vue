5de0c7121e7ac92816b8548c35713d64d0fcc3e9
Alsu <alsu.96@hotmail.fi>
2020-08-22 20:00:49 +0300

    Add colors to differentiate destructive buttons

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex flex-col items-center">
     <button
-      class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
+      class="block bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
       v-on:click="clearMarkers"
     >
       Clear Markers
     </button>
     <button
-      class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      class="block bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 my-6 rounded focus:outline-none focus:shadow-outline"
       v-on:click="share"
     >
       Share
